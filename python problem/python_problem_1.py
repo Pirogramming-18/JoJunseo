e76b9b048b28acbd35a70d45ec8b57188f2930fa
Junseo <monstazo911@gmail.com>
2023-01-05 22:08:40 +0900

    1-3

--- python problem/python_problem_1.py
+++ python problem/python_problem_1.py
@@ -1,2 +1,18 @@
+class NotInRange(Exception):
+    def __init__(self):
+        super().__init__('1,2,3 중 하나를 입력하세요')
+
 num=0
-int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+
+while True:
+    try:
+        count = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        if(not(1<=count<=3)):
+            raise NotInRange
+    except ValueError:
+        print('정수를 입력하세요')
+    except NotInRange as e:
+        print(e)
+    else:
+        break
+
