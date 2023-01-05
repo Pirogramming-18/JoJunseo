cc5c227e5960c30949b29b1a97f2eba61931eb47
Junseo <monstazo911@gmail.com>
2023-01-05 22:35:08 +0900

    1-8

--- python problem/python_problem_1.py
+++ python problem/python_problem_1.py
@@ -2,39 +2,41 @@ class NotInRange(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
 
-num=0
-player = 'A'
+def brGame():
+    num = 0
+    player = 'A'
 
-while True:
     while True:
-        try:
-            count = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-            if(not(1<=count<=3)):
-                raise NotInRange
-        except ValueError:
-            print('정수를 입력하세요')
-        except NotInRange as e:
-            print(e)
+        while True:
+            try:
+                count = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+                if (not (1 <= count <= 3)):
+                    raise NotInRange
+            except ValueError:
+                print('정수를 입력하세요')
+            except NotInRange as e:
+                print(e)
+            else:
+                break
+
+        while count > 0:
+            num += 1
+            print('player{0} : {1}'.format(player, num))
+            if num == 31:
+                break
+            count -= 1
+
+        if player == 'A':
+            player = 'B'
         else:
+            player = 'A'
+
+        if num == 31:
             break
 
-    while count>0:
-        num += 1
-        if num>31 :
-            break
-        print('player{0} : {1}'.format(player,num))
-        count -= 1
+    print('player{0} win!'.format(player))
 
-    print()
 
-    if player == 'A':
-        player = 'B'
-    else :
-        player = 'A'
 
-    if num>31:
-        break
-
-print('player{0} win!'.format(player))
-
+brGame()
 
