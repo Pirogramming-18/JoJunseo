f6e9f0f028a01851d94ddf21ca35fbf7d332354f
Junseo <monstazo911@gmail.com>
2023-01-10 20:10:56 +0900

    Django 설치하기

---
+++ djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/monstazo/Workspace/pirogramming/JoJunseo/djangogirls/myvenv/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
